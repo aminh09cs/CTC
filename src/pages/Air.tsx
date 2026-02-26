292303354e5c8aaaaeb0626155ca05b767c1e297
aminh09cs <18521098@gm.uit.edu.vn>
2026-02-26 20:35:04 +0700

    feat: add draft chart

--- src/pages/Air.tsx
+++ src/pages/Air.tsx
@@ -1,7 +1,15 @@
 export function Air() {
   return (
-    <div className="flex min-h-[calc(100vh-3.5rem)] items-center justify-center">
-      <span className="text-8xl font-bold text-muted-foreground">A</span>
+    <div className="flex min-h-[calc(100vh-3.5rem)] w-full flex-col p-4">
+      <iframe
+        title="BBSkillsSP"
+        width="100%"
+        height="541.25"
+        src="https://app.powerbi.com/reportEmbed?reportId=9c1cff04-6802-47e8-8aba-1355374bbb17&autoAuth=true&ctid=6d1dde6a-207f-4173-acb7-9d5222a783a1"
+        frameBorder="0"
+        allowFullScreen
+        className="min-h-[calc(100vh-6rem)] rounded-lg"
+      />
     </div>
   )
 }
